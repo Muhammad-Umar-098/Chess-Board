444ed5bc51b93569f66c4df776f6f1224303e41d
“[hamza <jdrajput19@gmail.com>
2024-10-07 20:15:37 +0500

    Chess Done

---
+++ chessboard/lib/main.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("ChessBoard"),
+          centerTitle: true,
+          backgroundColor: Colors.amberAccent,
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
